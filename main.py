20c11cd5aa048981ebe38b74e9d8a0d08f910e77
lgf4591 <lgf4591@outlook.com>
2024-02-06 21:14:11 +0800

    feat: add  source/chromego_merge

--- main.py
+++ main.py
@@ -26,6 +26,11 @@ freefq_doc = curl('https://raw.githubusercontent.com/freefq/free/master/README.m
 github520_doc = curl('https://raw.githubusercontent.com/521xueweihan/GitHub520/main/README.md')
 fetch_github_hosts_doc = curl('https://raw.githubusercontent.com/Licoy/fetch-github-hosts/main/README.md')
 
+clashmeta_without_warp = curl('https://mareep.netlify.app/sub/merged_proxies_new.yaml')
+clashmeta_with_warp = curl('https://mareep.netlify.app/sub/merged_warp_proxies_new.yaml')
+shadowrocket_and_nekoray = curl('https://mareep.netlify.app/sub/shadowrocket_base64.txt')
+sing_box = curl('https://mareep.netlify.app/sub/sb.json')
+
 
 with open("python/chromego_merge/ReadMe.md","r", encoding="utf-8") as f:
     chromego_merge_readme = f.read()
@@ -35,20 +40,20 @@ with open("python/chromego_merge/sub/merged_proxies.yaml","r", encoding="utf-8")
 
 with open("python/chromego_merge/sub/merged_proxies_new.yaml","r", encoding="utf-8") as f:
     chromego_merge_merged_proxies_new = f.read()
-    
+
 with open("python/chromego_merge/sub/merged_warp_proxies.yaml","r", encoding="utf-8") as f:
     chromego_merge_merged_warp_proxies = f.read()
-    
+
 with open("python/chromego_merge/sub/merged_warp_proxies_new.yaml","r", encoding="utf-8") as f:
     chromego_merge_merged_warp_proxies_new = f.read()
-    
+
 with open("python/chromego_merge/sub/shadowrocket_base64.txt","r", encoding="utf-8") as f:
     chromego_merge_shadowrocket_base64 = f.read()
 
 
 final_content = f"""
 ---
-title: VPN合集 
+title: VPN合集
 date: {update_time}
 index_img: https://fluid.s3.bitiful.net/hello-fluid/cover.png?w=480&fmt=webp
 category: VPN
@@ -132,7 +137,19 @@ def main():
     # print(final_content)
     with open("source/_posts/VPN合集.md", "w", encoding="utf-8") as f:
         f.write(final_content)
-    
+
+		with open("source/chromego_merge/merged_proxies_new.yaml", "w", encoding="utf-8") as f:
+        f.write(clashmeta_without_warp)
+
+		with open("source/chromego_merge/merged_warp_proxies_new.yaml", "w", encoding="utf-8") as f:
+        f.write(clashmeta_with_warp)
+
+		with open("source/chromego_merge/shadowrocket_base64.txt", "w", encoding="utf-8") as f:
+        f.write(shadowrocket_and_nekoray)
+
+		with open("source/chromego_merge/sb.json", "w", encoding="utf-8") as f:
+        f.write(sing_box)
+
 
 if __name__ == "__main__":
     main()
